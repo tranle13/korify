ca3c619d3717394cbebe13187329805a4b875225
Sunny Le <letutran98@gmail.com>
2024-03-28 21:07:36 -0400

    Remove image upload, save it for the future devexit

--- Korify/AddGroupViewController.swift
+++ Korify/AddGroupViewController.swift
@@ -6,27 +6,16 @@
 //
 
 import UIKit
-import Photos
-import PhotosUI
 
-class AddGroupViewController: UIViewController, UITextFieldDelegate, PHPickerViewControllerDelegate {
+class AddGroupViewController: UIViewController, UITextFieldDelegate {
   
   @IBOutlet weak var nameTF: UITextField!
   @IBOutlet weak var descriptionTF: UITextField!
-  
-  var imageConfigs = PHPickerConfiguration()
   
   override func viewDidLoad() {
     super.viewDidLoad()
     nameTF.delegate = self
     descriptionTF.delegate = self
-    imageConfigs.filter = .images
-  }
-
-  @IBAction func uploadCoverTapped(_ sender: UIButton) {
-    let picker = PHPickerViewController(configuration: imageConfigs)
-    picker.delegate = self
-    present(picker, animated: true, completion: nil)
   }
 
   @IBAction func saveTapped(_ sender: UIButton) {
@@ -41,11 +30,5 @@ class AddGroupViewController: UIViewController, UITextFieldDelegate, PHPickerVie
         nameTF.becomeFirstResponder()
     }
     return true
-  }
-  
-  func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
-    picker.dismiss(animated: true, completion: nil)
-    
-    dump(results)
   }
 }
