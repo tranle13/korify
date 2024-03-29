a99244b6acff29ed2eb49ab627a7e2a9801c6ceb
Sunny Le <letutran98@gmail.com>
2024-03-28 21:01:42 -0400

    Allowed user to select picture from photo gallery

--- Korify/AddGroupViewController.swift
+++ Korify/AddGroupViewController.swift
@@ -6,19 +6,27 @@
 //
 
 import UIKit
+import Photos
+import PhotosUI
 
-class AddGroupViewController: UIViewController, UITextFieldDelegate {
-
+class AddGroupViewController: UIViewController, UITextFieldDelegate, PHPickerViewControllerDelegate {
+  
   @IBOutlet weak var nameTF: UITextField!
   @IBOutlet weak var descriptionTF: UITextField!
+  
+  var imageConfigs = PHPickerConfiguration()
   
   override func viewDidLoad() {
     super.viewDidLoad()
     nameTF.delegate = self
     descriptionTF.delegate = self
+    imageConfigs.filter = .images
   }
 
   @IBAction func uploadCoverTapped(_ sender: UIButton) {
+    let picker = PHPickerViewController(configuration: imageConfigs)
+    picker.delegate = self
+    present(picker, animated: true, completion: nil)
   }
 
   @IBAction func saveTapped(_ sender: UIButton) {
@@ -33,5 +41,11 @@ class AddGroupViewController: UIViewController, UITextFieldDelegate {
         nameTF.becomeFirstResponder()
     }
     return true
+  }
+  
+  func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+    picker.dismiss(animated: true, completion: nil)
+    
+    dump(results)
   }
 }
